d4661cf591e9d59e8935c09bb6c01651811bed08
xu <2813519505@qq.com>
2026-06-09 16:57:34 +0800

    python

---
+++ study01/08.python.py
@@ -0,0 +1,23 @@
+from selectors import SelectSelector
+from traceback import print_tb
+
+#if 条件判断
+score=60
+
+if score>=60:
+    print("大于60")
+
+#if else
+if score>=70:
+    print("大于70")
+else:
+    print("小于70")
+
+
+#if elif else
+if score>=80:
+    print("大于80")
+elif score>=90:
+    print("大于90")
+else:
+    print("大于100")
